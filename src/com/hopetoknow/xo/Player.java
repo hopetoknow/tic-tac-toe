8429a7f821709e4ad397897fe57b91068db217e8
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-14 20:09:25 +0300

    add Player

---
+++ src/com/hopetoknow/xo/Player.java
@@ -0,0 +1,21 @@
+package com.hopetoknow.xo;
+
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
