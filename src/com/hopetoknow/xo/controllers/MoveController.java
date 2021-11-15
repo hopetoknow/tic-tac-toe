578d84735a753e2077faa055e3a7ba890c0314b7
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-15 21:34:01 +0300

    add Game & MoveController

---
+++ src/com/hopetoknow/xo/controllers/MoveController.java
@@ -0,0 +1,7 @@
+package com.hopetoknow.xo.controllers;
+
+public class MoveController {
+    public void applyFigure() {
+
+    }
+}
---
+++ src/com/hopetoknow/xo/model/Game.java
@@ -0,0 +1,28 @@
+package com.hopetoknow.xo.model;
+
+public class Game {
+
+    private final Player[] players;
+
+    private final Field field;
+
+    private final String name;
+
+    public Game(Player[] players, Field field, String name) {
+        this.players = players;
+        this.field = field;
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
