88472933c214c3c0881c517c1e0455659a85f1a3
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-15 14:21:20 +0300

    add InvalidPointException & AbstractXOException

---
+++ src/com/hopetoknow/xo/model/exceptions/AbstractXOException.java
@@ -0,0 +1,4 @@
+package com.hopetoknow.xo.model.exceptions;
+
+public abstract class AbstractXOException extends Exception {
+}
---
+++ src/com/hopetoknow/xo/model/exceptions/InvalidPointException.java
@@ -0,0 +1,4 @@
+package com.hopetoknow.xo.model.exceptions;
+
+public class InvalidPointException extends AbstractXOException {
+}
