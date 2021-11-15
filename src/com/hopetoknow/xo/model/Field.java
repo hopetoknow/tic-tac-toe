48ee0cb478a35b0d27a8bb20d8099000bb469e79
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-15 17:55:38 +0300

    update Field

--- src/com/hopetoknow/xo/model/Field.java
+++ src/com/hopetoknow/xo/model/Field.java
@@ -1,10 +1,15 @@
 package com.hopetoknow.xo.model;
 
+import com.hopetoknow.xo.model.exceptions.InvalidPointException;
+import com.hopetoknow.xo.model.exceptions.PointAlreadyOccupiedException;
+
 import java.awt.*;
 
 public class Field {
 
     public static final int FIELD_SIZE = 3;
+
+    public static final int MIN_COORDINATE = 0;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -12,11 +17,28 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(Point point) {
+    public Figure getFigure(Point point) throws InvalidPointException {
+        if (!isPointValid(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(Point point, Figure figure) {
+    public void setFigure(Point point, Figure figure) throws InvalidPointException, PointAlreadyOccupiedException {
+        if (!isPointValid(point)) {
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new PointAlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean isPointValid(Point point) {
+        return isCoordinateValid(point.x) && isCoordinateValid(point.y);
+    }
+
+    private boolean isCoordinateValid(int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < FIELD_SIZE;
     }
 }
