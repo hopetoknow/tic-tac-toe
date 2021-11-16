db508a900642878fb9679af4a9681c7b0f513991
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2021-11-16 14:33:37 +0300

    update MoveController with applyFigure method

--- src/com/hopetoknow/xo/controllers/MoveController.java
+++ src/com/hopetoknow/xo/controllers/MoveController.java
@@ -1,7 +1,20 @@
 package com.hopetoknow.xo.controllers;
 
-public class MoveController {
-    public void applyFigure() {
+import com.hopetoknow.xo.model.Field;
+import com.hopetoknow.xo.model.Figure;
+import com.hopetoknow.xo.model.exceptions.AbstractXOException;
+import com.hopetoknow.xo.model.exceptions.InvalidPointException;
+import com.hopetoknow.xo.model.exceptions.PointAlreadyOccupiedException;
 
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(Field field, Point point, Figure figure) throws InvalidPointException, PointAlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new PointAlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
     }
+
 }
